e36c7aff715abebec0cea74160acb393517dc3ac
Sshaya <asksbj002@gmail.com>
2019-05-21 15:03:03 -0700

    update heroes model

--- gacha_simulator/feh/models.py
+++ gacha_simulator/feh/models.py
@@ -48,7 +48,7 @@ class Heroes(models.Model):
     WEAPON_CHOICES = (
         (WEAPON_SWORD, 'sword'),
         (WEAPON_LANCE, 'lance'),
-        (WEAPON_AXE,'axe'),
+        (WEAPON_AXE, 'axe'),
         (WEAPON_RED_BOW, 'red_bow'),
         (WEAPON_BLUE_BOW, 'blue_bow'),
         (WEAPON_GREEN_BOW, 'green_bow'),
@@ -82,15 +82,27 @@ class Heroes(models.Model):
         (MOVE_FLYING, 'flying'),
     )
 
+    COLOR_WEAPON_MAPPING = {
+        TYPE_RED: [WEAPON_SWORD, WEAPON_RED_BOW, WEAPON_RED_DRAGGER, WEAPON_RED_TOME, WEAPON_RED_BREATH,
+                   WEAPON_RED_BEAST],
+        TYPE_BLUE: [WEAPON_LANCE, WEAPON_BLUE_BOW, WEAPON_BLUE_DRAGGER, WEAPON_BLUE_TOME, WEAPON_BLUE_BREATH,
+                    WEAPON_BLUE_BEAST],
+        TYPE_GREEN: [WEAPON_AXE, WEAPON_GREEN_BOW, WEAPON_GREEN_DRAGGER, WEAPON_GREEN_TOME, WEAPON_GREEN_BREATH,
+                     WEAPON_GREEN_BEAST],
+        TYPE_COLORLESS: [WEAPON_STAFF, WEAPON_COLORLESS_BOW, WEAPON_COLORLESS_DRAGGER, WEAPON_COLORLESS_BREATH,
+                         WEAPON_COLORLESS_BEAST]
+    }
+
     name = models.CharField(max_length=64, default=None, null=True)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES, default=None, null=True)
     rarity_low = models.IntegerField(default=None, null=True)
     rarity_high = models.IntegerField(default=None, null=True)
-    type = models.CharField(max_length=16, choices=TYPE_CHOICES, default=None, null=True)
+    hero_type = models.CharField(max_length=16, choices=TYPE_CHOICES, default=None, null=True)
     weapon_type = models.CharField(max_length=32, choices=WEAPON_CHOICES, default=None, null=True)
     move_type = models.CharField(max_length=16, choices=MOVE_CHOICES, default=None, null=True)
     appears_in = models.CharField(max_length=128, default=None, null=True)
     voice_actor = models.CharField(max_length=32, default=None, null=True)
+    art_by = models.CharField(max_length=32, default=None, null=True)
     release_date = models.DateTimeField(default=None, null=True)
     description = models.TextField(default=None, null=True)
     health = models.IntegerField(default=None, null=True)
