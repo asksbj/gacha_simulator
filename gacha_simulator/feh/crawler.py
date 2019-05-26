adf04609a41cf55d3859e8f9f24f014a81c1a88d
asksbj <dongsj108@hotmail.com>
2019-05-25 18:01:19 -0700

    add feh pools crawler

--- gacha_simulator/feh/crawler.py
+++ gacha_simulator/feh/crawler.py
@@ -1,14 +1,16 @@
 import re
 import html
 
+from django.db import transaction
+
 from commons.crawler import open_url, save_images
-from .models import Heroes
+from .models import Heroes, Pools
+
+BASE_URL = 'https://feheroes.gamepedia.com/'
 
 
 def crawl_heroes():
-    base_url = 'https://feheroes.gamepedia.com/'
-
-    hero_list_url = base_url + 'List_of_Heroes'
+    hero_list_url = BASE_URL + 'List_of_Heroes'
     hero_list_result = open_url(hero_list_url).decode('utf-8')
 
     hero_list_patten = re.compile(r'<td><a href="/([^"]+)" title="([^"]+)"><img alt="([^"]+)" src="([^"]+)" ')
@@ -22,7 +24,7 @@ def crawl_heroes():
 
         columns['image'] = html.unescape(image_name)
 
-        hero_url = base_url + hero_url
+        hero_url = BASE_URL + hero_url
 
         hero_result = open_url(hero_url).decode('utf-8')
 
@@ -52,10 +54,69 @@ def crawl_heroes():
 
         hero_name = html.unescape(hero_name)
         print('Processing hero {}'.format(hero_name))
-        Heroes.objects.update_or_create(name=hero_name, defaults=columns)
+        with transaction.atomic():
+            Heroes.objects.update_or_create(name=hero_name, defaults=columns)
 
-        save_images('feh', image_name, image_address)
+            save_images('feh', image_name, image_address)
 
+
+def crawl_pools():
+    pool_list_url = BASE_URL + 'Summoning_Focus_archive'
+    pool_list_result = open_url(pool_list_url).decode('utf-8')
+
+    pool_list_patten = re.compile(
+        r'style="text-align:center;width:33%"><a href="/([^"]+)" title="([^"]+)"><img alt="([^"]+)" src="([^"]+)" ')
+
+    items = pool_list_patten.findall(pool_list_result)
+    for item in items:
+        columns = {}
+
+        pool_url, pool_name, pool_image_name, pool_image_address = item
+        pool_name = html.unescape(pool_name)
+
+        columns['image'] = html.unescape(pool_image_name)
+
+        pool_url = BASE_URL + pool_url
+
+        pool_result = open_url(pool_url).decode('utf-8')
+
+        # print(pool_result)
+
+        rarity_parttern = re.compile(
+            r'<td>5★ Focus</td><td>([^%"]+)%</td></tr><tr><td>5★</td><td>([^%"]+)%</td></tr><tr><td>4★</td><td>([^%"]+)'
+            r'%</td></tr><tr><td>3★</td><td>([^%"]+)%</td></tr></tbody>')
+        rarity_items = rarity_parttern.findall(pool_result)
+        if not rarity_items:
+            continue
+        rarity_item = rarity_items[-1]
+
+        start5_focus, start5, start4, start3 = rarity_item
+
+        columns['start5_focus'] = float(re.sub('[^0-9.]', '0', start5_focus))/100
+        columns['start5'] = float(re.sub('[^0-9.]', '0', start5)) / 100
+        columns['start4'] = float(re.sub('[^0-9.]', '0', start4)) / 100
+        columns['start3'] = float(re.sub('[^0-9.]', '0', start3)) / 100
+
+        pool_hero_patten = re.compile(r'</span><a href="/([^"]+)" title="([^"]+)"><img alt="" src="([^"]+)" ')
+
+        hero_items = pool_hero_patten.findall(pool_result)
+
+        hero_set = set()
+        for hero_item in hero_items:
+            hero_url, hero_name, image_address = hero_item
+
+            hero_set.add(html.unescape(hero_name))
+
+        print('Processing pool {}'.format(pool_name))
+        with transaction.atomic():
+            pool, created = Pools.objects.update_or_create(name=pool_name, defaults=columns)
+
+            if created:
+                for hero_name in hero_set:
+                    hero = Heroes.objects.get(name=hero_name)
+                    pool.heroes.add(hero)
+
+            save_images('feh', pool_image_name, pool_image_address)
 
 
 
--- gacha_simulator/feh/management/commands/feh_crawler.py
+++ gacha_simulator/feh/management/commands/feh_crawler.py
@@ -1,11 +1,17 @@
 from django.core.management.base import BaseCommand
 
-from feh.crawler import crawl_heroes
+from feh.crawler import crawl_heroes, crawl_pools
 
 
 class Command(BaseCommand):
     def __init__(self):
         super(Command, self).__init__('test_crawler')
 
+    def add_arguments(self, parser):
+        parser.add_argument('--target', type=str, dest='target', default='hero', help='Crawler Target')
+
     def handle(self, *args, **options):
-        crawl_heroes()
+        if options['target'] == 'hero':
+            crawl_heroes()
+        elif options['target'] == 'pool':
+            crawl_pools()
